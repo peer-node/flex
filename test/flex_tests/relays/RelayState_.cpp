1d3a0fa4aacdd1c5281924a9407007260124c88b
peernode <peernode@gmx.com>
2016-08-22 17:33:39 +0100

    add scheduler tests & remove unused code

--- test/flex_tests/relays/RelayState_.cpp
+++
@@ -1,6 +0,0 @@
-#include "RelayState_.h"
-
-void RelayState_::AddRelay(Relay relay)
-{
-    relays.push_back(relay);
-}
---
+++ test/flex_tests/scheduler/test_scheduler.cpp
@@ -0,0 +1,42 @@
+#include "gmock/gmock.h"
+#include "../flex_data/TestData.h"
+#include "flexnode/schedule.h"
+
+using namespace ::testing;
+
+void SetDone(uint160 hash)
+{
+    scheduledata[hash]["done"] = 1;
+}
+
+class AScheduler : public Test
+{
+public:
+    Scheduler scheduler;
+    int64_t start_time = 1000000000;
+    int64_t task_time = start_time + 1000000;
+    uint160 hash = 1;
+
+    virtual void SetUp()
+    {
+        SetMockTimeMicros(start_time);
+    }
+};
+
+TEST_F(AScheduler, CanScheduleEvents)
+{
+    scheduler.AddTask(ScheduledTask("set_done", SetDone));
+    scheduler.Schedule("set_done", hash, task_time);
+
+    SetMockTimeMicros(task_time - 1);
+    scheduler.DoTasksScheduledForExecutionBeforeNow();
+
+    int is_done = scheduledata[hash]["done"];
+    ASSERT_THAT(is_done, Eq(0));
+
+    SetMockTimeMicros(task_time);
+    scheduler.DoTasksScheduledForExecutionBeforeNow();
+
+    is_done = scheduledata[hash]["done"];
+    ASSERT_THAT(is_done, Eq(1));
+}
